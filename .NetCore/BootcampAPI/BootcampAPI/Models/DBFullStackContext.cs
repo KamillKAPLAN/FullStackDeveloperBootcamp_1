c958f6a4b843db8b944dc9452acfc91636a93c28
KAMILKAPLAN\KAMILKAPLAN <kamilkaplnn@gmail.com>
2022-11-05 19:13:33 +0300

    fluent api

--- .NetCore/BootcampAPI/BootcampAPI/Models/DBFullStackContext.cs
+++ .NetCore/BootcampAPI/BootcampAPI/Models/DBFullStackContext.cs
@@ -17,5 +17,34 @@ namespace BootcampAPI.Models
                 optionsBuilder.UseSqlServer("DATA SOURCE=KAMILKAPLAN;DATABASE=DBFullStack;UID=sa;PWD=1");
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // modelBuilder.HasDefaultSchema("SchemaName");
+            // modelBuilder.Entity<Student>().ToTable("TableName");
+            //modelBuilder.Entity<Student>().Property(x => x.SID).HasColumnName("").HasComment("");
+            //modelBuilder.Entity<Student>().Property(x => x.SID).HasColumnName("").HasComment("");
+            //modelBuilder.Entity<Student>().Property(x => x.SID).HasColumnName("").HasComment("");
+            //modelBuilder.Entity<Student>().Property(x => x.SID).HasColumnName("").HasComment("");
+            //modelBuilder.Entity<Student>().Property(x => x.SID).HasColumnName("").HasComment("");
+            //modelBuilder.Entity<Student>().Property(x => x.SID).HasColumnName("").HasComment("");
+            //modelBuilder.Entity<Student>().Property(x => x.SID).HasColumnName("").HasComment("");
+            //modelBuilder.Entity<Student>().Property(x => x.SID).HasColumnName("").HasComment("");
+            //modelBuilder.Entity<Student>().Property(x => x.SID).HasColumnName("").HasComment("");
+            //modelBuilder.Entity<Student>().Property(x => x.SID).HasColumnName("").HasComment("");
+            //modelBuilder.Entity<Student>().Property(x => x.SID).HasColumnName("").HasComment("");
+            //modelBuilder.Entity<Student>().Property(x => x.SID).HasColumnName("").HasComment("");
+            modelBuilder.Entity<Student>(entity =>
+            {
+                // entity.ToTable("...");
+                // entity.ToTable("TableName", "SchemaName");
+                entity.HasKey(x => x.SID);
+                entity.Property(x => x.StudentName).HasColumnName("Name").HasMaxLength(25);
+                entity.Property(x => x.Height)
+                .IsRequired()
+                .HasDefaultValue("1.1")
+                .HasColumnType("float")
+                .HasComputedColumnSql("decimal(14,2)");
+            });
+        }
     }
 }
